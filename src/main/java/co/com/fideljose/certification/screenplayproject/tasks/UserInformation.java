052ac721aae19b9311fb545b4bf4be60901ecbd8
Fidel Jose Villero de Hoyos <fvillero@bancolombia.com.co>
2021-06-13 14:24:32 -0500

    Complete set variables less Country options

--- src/main/java/co/com/fideljose/certification/screenplayproject/tasks/UserInformation.java
+++ src/main/java/co/com/fideljose/certification/screenplayproject/tasks/UserInformation.java
@@ -13,24 +13,27 @@ import java.util.List;
 
 public class UserInformation implements Task {
 
-    private String name;
-    private String email;
-    private String password;
-    private String confirmPassword;
+    private List<User> users;
 
     public UserInformation(List<User> users){
-        this.name            = users.get(0).getFirstName();
-        this.email           = users.get(0).getEmail();
-        this.password        = users.get(0).getPassword();
-        this.confirmPassword = users.get(0).getConfirmPassword();
+        this.users = users;
     }
 
     @Override
     public <T extends Actor> void performAs(T actor) {
-        actor.attemptsTo(Enter.theValue(this.name).into(RegisterUserAccount.INPUT_USERNAME));
-        actor.attemptsTo(Enter.theValue(this.email).into(RegisterUserAccount.INPUT_EMAIL));
-        actor.attemptsTo(Enter.theValue(this.password).into(RegisterUserAccount.INPUT_PASSWORD));
-        actor.attemptsTo(Enter.theValue(this.confirmPassword).into(RegisterUserAccount.INPUT_CONFIRMPASSWORD));
+        actor.attemptsTo(Enter.theValue(this.users.get(0).getUsername()).into(RegisterUserAccount.INPUT_USERNAME));
+        actor.attemptsTo(Enter.theValue(this.users.get(0).getEmail()).into(RegisterUserAccount.INPUT_EMAIL));
+        actor.attemptsTo(Enter.theValue(this.users.get(0).getPassword()).into(RegisterUserAccount.INPUT_PASSWORD));
+        actor.attemptsTo(Enter.theValue(this.users.get(0).getConfirmPassword()).into(RegisterUserAccount.INPUT_CONFIRMPASSWORD));
+        actor.attemptsTo(Enter.theValue(this.users.get(0).getFirstName()).into(RegisterUserAccount.INPUT_FIRSTNAME));
+        actor.attemptsTo(Enter.theValue(this.users.get(0).getLastName()).into(RegisterUserAccount.INPUT_LASTNAME));
+        actor.attemptsTo(Enter.theValue(this.users.get(0).getPhoneNumber()).into(RegisterUserAccount.INPUT_PHONE));
+
+//        actor.attemptsTo(Enter.theValue(this.users.get(0).getCountry()).into(RegisterUserAccount.INPUT_COUNTRY));
+        actor.attemptsTo(Enter.theValue(this.users.get(0).getCity()).into(RegisterUserAccount.INPUT_CITY));
+        actor.attemptsTo(Enter.theValue(this.users.get(0).getAddress()).into(RegisterUserAccount.INPUT_ADDRESS));
+        actor.attemptsTo(Enter.theValue(this.users.get(0).getRegion()).into(RegisterUserAccount.INPUT_STATE));
+        actor.attemptsTo(Enter.theValue(this.users.get(0).getPostalCode()).into(RegisterUserAccount.INPUT_CODEPOSTAL));
     }
 
     public static UserInformation setUserInformation(List<User> users){
